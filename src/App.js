e705acfb30540750f6cea72cbd4913baea46c6c7
miroslava1987 <miroslava1987@gmail.com>
2020-10-08 00:09:11 +0300

    install node-sass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import { Wrapper } from "./components/Wrapper/Wrapper"
 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+        <Wrapper />
+    );
 
 }
 
