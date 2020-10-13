f85190417a46cf5385e72a62064a9164fa98a83a
miroslava1987 <miroslava1987@gmail.com>
2020-10-13 04:06:15 +0300

    merge dev

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import './styles/style.scss';
-import { Wrapper } from "./components/Wrapper/Wrapper";
-
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
-function App() {
-    return (
-        <Wrapper />
-    );
+import { Button } from './components/Button';
+import { Modal } from './components/Modal';
+import { ModalThanks } from './components/Modal/ModalThanks';
+import { closeModal, openModal } from './store/modal/actions';
+import { selectModalContent, selectModalIsOpen } from './store/modal/selectors';
 
+function App () {
+  const dispatch = useDispatch();
+  const modalIsOpen = useSelector(selectModalIsOpen);
+  const modalContent = useSelector(selectModalContent);
+
+  return (
+    <div className="App">
+      {modalIsOpen && <Modal {...modalContent} closeModalHandler={() => dispatch(closeModal())}/>}
+      
+      <Button text="БЕЛАЯ КНОПКА" color='white' onClick={() => dispatch(openModal(ModalThanks))}/>
+    </div>
+  );
 }
 
 export default App;
