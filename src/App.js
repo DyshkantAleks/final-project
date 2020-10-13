76d1d0e2fed0f24ca06a546a37cb7a71255834df
Victoria Medvedeva <medvedeva.ufa@gmail.com>
2020-10-13 11:47:34 +0300

    Current changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import './App.css';
 import './styles/style.scss';
 
 
+import {Button} from './components/Button';
+import {Modal} from './components/Modal';
+import {ModalThanks} from './components/Modal/ModalThanks';
+import {closeModal, openModal} from './store/modal/actions';
+import {selectModalContent, selectModalIsOpen} from './store/modal/selectors';
+import {Navigation} from "./pages/navigation";
 
+function App() {
+    const dispatch = useDispatch();
+    const modalIsOpen = useSelector(selectModalIsOpen);
+    const modalContent = useSelector(selectModalContent);
 
-import { Button } from './components/Button';
-import { Modal } from './components/Modal';
-import { ModalThanks } from './components/Modal/ModalThanks';
-import { closeModal, openModal } from './store/modal/actions';
-import { selectModalContent, selectModalIsOpen } from './store/modal/selectors';
-
-function App () {
-  const dispatch = useDispatch();
-  const modalIsOpen = useSelector(selectModalIsOpen);
-  const modalContent = useSelector(selectModalContent);
-
-  return (
-    <div className="App">
-      {modalIsOpen && <Modal {...modalContent} closeModalHandler={() => dispatch(closeModal())}/>}
-
-      <Button text="БЕЛАЯ КНОПКА" color='white' onClick={() => dispatch(openModal(ModalThanks))}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            
+        </div>
+    );
 }
 
 export default App;
--- src/commons/Header/Header.jsx
+++ src/commons/Header/Header.jsx
@@ -28,8 +28,6 @@ export const Header = () => {
                 {width >= 1200 && (
                     <CatalogList/>
                 )}
-
-
         </>
     )
 }
