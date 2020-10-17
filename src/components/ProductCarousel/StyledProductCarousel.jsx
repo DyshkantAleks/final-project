48d1454f5248eb1a98af63ad268f4bf952572344
miroslava1987 <62344060+miroslava1987@users.noreply.github.com>
2020-10-17 19:24:47 +0300

    Mira. add media in swiper (#23)
    * creating a structure in a database
    
    * creating a structure of img in a database
    
    * add new category in DB
    
    * merge dev
    
    * add all category of img in DB
    
    * Add files via upload
    
    * Update .travis.yml (#8)
    
    * Update .travis.yml (#9)
    
    * add product cart
    
    * fixed app
    
    * install node-sass
    
    * install styled-components
    
    * add swiper in ProductCarousel
    
    * fixed package.json
    
    * change direction in swiper
    
    * fixed ProductItemDetails
    
    * add useToggle for FontAwesomeIcon
    
    * add PopularProductList in product page
    
    * install react-redux
    
    * merge dev
    
    * fixed merge conflicts
    
    * add button in component
    
    * add button in component
    
    * add button in component
    
    * initial commit
    
    * merge branch dev into Mira
    
    * add change in swiper styles
    
    * clear App.js
    
    * cleare space
    
    * fixed package.json
    
    * add ProductPage in router, updated styles, add favorite item. Work in progress
    
    * delete travis & fixed styles in swiper
    
    * add media in ProductItem
    
    * fixed thumbs in swiper
    
    * fixed swiper-slider
    
    * add media in swiper
    
    * add Navigation in App
    
    * fixed ProductItem after merge dev
    
    * change colorTheme
    
    Co-authored-by: Dyshkant Oleksandr <48221137+DyshkantAleks@users.noreply.github.com>

--- src/components/ProductCarousel/StyledProductCarousel.jsx
+++ src/components/ProductCarousel/StyledProductCarousel.jsx
@@ -1,38 +1,81 @@
 import styled from 'styled-components';
+import {device} from "../../styles/breakpoints/breakpoints";
 
 export const SwiperContainer = styled.div`
 display: flex;
 flex-flow: row;
 `;
+export const SwiperContainerHorizontal = styled.div`
+display: flex;
+flex-direction: column;
+`;
 
 export const SwiperThumbs = styled.div`
 width: 15%;
+padding-right: 3rem;
+`;
+export const SwiperThumbsHorizontal = styled.div`
+width: 100%;
 `;
 
 export const SwiperGallery = styled.div`
-width: 85%;
+width: 80%;
+`;
+export const SwiperGalleryHorizontal = styled.div`
+width: 100%;
 `;
 
 export const ImageMainContainer = styled.div`
-flex-basis: 48.5rem;
+display: flex;
+justify-content: center;
+align-items: center;
+width: 99%;
+height: 28rem;
+overflow: hidden;
+border: .1rem solid #e6e9ef;
+    @media ${device.tabletS}{
+        height: 55rem;
+    };
+    @media ${device.tabletM}{
+        height: 57.6rem;
+    }
+    @media ${device.tabletL}{
+        height: 51rem;
+    }
 `;
 
 export const ImageMain = styled.img`
 display: block;
 max-width: 100%;
-max-height: 100%;
+height: unset;
 margin: 0 auto;
+overflow: hidden;
+padding: .5rem;
 `;
 
 export const ImageThumbsContainer=styled.div`
-width: 7.8rem;
-height: 7.8rem;
+display: flex;
+justify-content: center;
+align-items: center;
+width: 98%;
+height: 7rem;
+border: .1rem solid #e6e9ef;
 overflow: hidden;
+@media ${device.tabletS}{
+    height: 9rem;
+};
+@media ${device.tabletM}{
+    height: 8rem;
+}
+@media ${device.tabletL}{
+    height: 8rem;
+}
 `;
 
 export const ImageThumbs = styled.img`
 display: block;
-max-width: 100%;
-height: auto;
+max-width: 99%;
+height: unset;
 margin: 0 auto;
+padding: .5rem;
 `;
