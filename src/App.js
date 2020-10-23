49265c3c6942af0f0f5cb8cf800ec18f85208c7c
miroslava1987 <miroslava1987@gmail.com>
2020-10-23 23:06:37 +0300

    add middleware in ProductItem

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import './App.css';
-
-import { ProductCarousel } from './components/ProductCarousel/ProductCarousel';
-import { ProductItem } from './components/ProductItem/ProductItem';
+import React from 'react';
 
 import { Navigation } from './pages/navigation';
-import { getProducts } from './store/products_draft/middlware';
 
 import './styles/style.scss';
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getProducts());
-    return () => {};
-  }, []);
-  // const modalIsOpen = useSelector(selectModalIsOpen);
-  // const modalContent = useSelector(selectModalContent);
-
   return (
-    <div className='App'>
-      <Navigation />
-    </div>
+    <Navigation />
   );
 }
 
--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+// import { withRouter } from 'react-router-dom';
 
 import { faHeart as fasFaHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as farFaHeart } from '@fortawesome/free-regular-svg-icons';
@@ -26,71 +26,49 @@ import {
 } from './StyledProductItem';
 import { getProductList } from '../../store/products_draft/actions';
 import { selectProducts, selectProductItem } from '../../store/products_draft/selectors';
-
+import { getProducts } from '../../store/products_draft/middlware';
 const ProductItem = (props) => {
 
   const products = useSelector(selectProducts);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getProductList());
+    dispatch(getProducts());
   }, [dispatch]);
 
-  const productItem = useSelector(selectProductItem);
+  console.log(products)
+  // const productItem = useSelector(selectProductItem);
 
-  const getProductItemList = (productObj) => {
-    const objCopy = JSON.parse(JSON.stringify(productObj));
-    let productArray = [];
-    for (const key in objCopy) {
-      productArray = [...productArray, ...objCopy[key]]
-    }
-    return productArray
-  }
+  // const getProductItemList = (productObj) => {
+  //   const objCopy = JSON.parse(JSON.stringify(productObj));
+  //   let productArray = [];
+  //   for (const key in objCopy) {
+  //     productArray = [...productArray, ...objCopy[key]]
+  //   }
+  //   return productArray
+  // }
 
-  const productList = getProductItemList(products, productItem);
+  // const productList = getProductItemList(products, productItem);
 
   const [inFavorite, toggleInFavorite] = useToggle();
   return (
     <>
       {
-        productList.map((item, index) =>
+        products.map((item, index) =>
           <ConteinerItem key={index}>
             <PhotoBox>
-              {/* <StyledLink to={`product/${item.route}`}> */}
-              <Photo alt='our product' src={item.image[0]} onClick={() => props.history.push('/products/' + item.route)} />
-              {/* {isDiscount &&
-                <ProductActivityContainer>
-                  <IconContainer>
-                    <SaleIcon>
-                      {ProductActivity.sale}
-                    </SaleIcon>
-                 </IconContainer>
-                </ProductActivityContainer>}
-              {isNew && <ProductActivityContainer>
-                <IconContainer>
-                 <NewIcon>
-                   {ProductActivity.new}
-                 </NewIcon>
-               </IconContainer>
-             </ProductActivityContainer>}
-             {isTopRated && <ProductActivityContainer>
-                <IconContainer>
-                 <TopRatedIcon>
-                   {ProductActivity.topMain}
-                 </TopRatedIcon>
-               </IconContainer>
-             </ProductActivityContainer>} */}
-              {/* </StyledLink> */}
+              <Photo alt={item.name} src={item.image} />
             </PhotoBox>
             <TitleBox>
               {/* <StyledLink to={`product/${item.route}`}> */}
               <NameContainer>
-                <Name onClick={() => props.history.push('/products/' + item.route)}>{item.name}</Name>
+                {/* <Name onClick={() => props.history.push('/products/' + item.route)}>{item.name}</Name> */}
+                <Name>{item.name}</Name>
               </NameContainer>
               {/* </StyledLink> */}
               {!inFavorite && <StyledFontAwesomeIcon icon={farFaHeart} onClick={toggleInFavorite} />}
               {inFavorite && <StyledFontAwesomeIcon icon={fasFaHeart} onClick={toggleInFavorite} />}
-              <Price>{item.price.toLocaleString()}</Price>
+              <Price>{item.currentPrice.toLocaleString()}</Price>
               {/* <Button text={'Купить'} /> */}
             </TitleBox>
           </ConteinerItem>
@@ -98,6 +76,55 @@ const ProductItem = (props) => {
       }
     </>
   )
+  // return (
+  //   <>
+  //     {
+  //       productList.map((item, index) =>
+  //         <ConteinerItem key={index}>
+  //           <PhotoBox>
+  //             {/* <StyledLink to={`product/${item.route}`}> */}
+  //             <Photo alt='our product' src={item.image[0]} onClick={() => props.history.push('/products/' + item.route)} />
+  //             {/* {isDiscount &&
+  //               <ProductActivityContainer>
+  //                 <IconContainer>
+  //                   <SaleIcon>
+  //                     {ProductActivity.sale}
+  //                   </SaleIcon>
+  //                </IconContainer>
+  //               </ProductActivityContainer>}
+  //             {isNew && <ProductActivityContainer>
+  //               <IconContainer>
+  //                <NewIcon>
+  //                  {ProductActivity.new}
+  //                </NewIcon>
+  //              </IconContainer>
+  //            </ProductActivityContainer>}
+  //            {isTopRated && <ProductActivityContainer>
+  //               <IconContainer>
+  //                <TopRatedIcon>
+  //                  {ProductActivity.topMain}
+  //                </TopRatedIcon>
+  //              </IconContainer>
+  //            </ProductActivityContainer>} */}
+  //             {/* </StyledLink> */}
+  //           </PhotoBox>
+  //           <TitleBox>
+  //             {/* <StyledLink to={`product/${item.route}`}> */}
+  //             <NameContainer>
+  //               <Name onClick={() => props.history.push('/products/' + item.route)}>{item.name}</Name>
+  //             </NameContainer>
+  //             {/* </StyledLink> */}
+  //             {!inFavorite && <StyledFontAwesomeIcon icon={farFaHeart} onClick={toggleInFavorite} />}
+  //             {inFavorite && <StyledFontAwesomeIcon icon={fasFaHeart} onClick={toggleInFavorite} />}
+  //             <Price>{item.price.toLocaleString()}</Price>
+  //             {/* <Button text={'Купить'} /> */}
+  //           </TitleBox>
+  //         </ConteinerItem>
+  //       )
+  //     }
+  //   </>
+  // )
 }
 
-export default withRouter(ProductItem);
+// export default withRouter(ProductItem);
+export default ProductItem;
