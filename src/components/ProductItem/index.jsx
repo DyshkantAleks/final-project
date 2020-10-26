131ed64788bdd46ff169caa9efbe8c223bb65e4c
miroslava1987 <miroslava1987@gmail.com>
2020-10-27 00:10:06 +0200

    fixed ProductItem

--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -9,18 +9,9 @@ import { IconSale } from './IconsSvg/IconSale';
 // import { IconTopRated } from './IconsSvg/IconTopRated';
 import { useToggle } from '../../utils/useToggle';
 import { ConteinerItem, PhotoBox, Photo, TitleBox, NameContainer, Name, Price, StyledLink, ProductActivityContainer } from './StyledProductItem';
-import { selectProducts } from '../../store/products_draft/selectors';
-import { getProducts } from '../../store/products_draft/middlware';
 
 const ProductItem = (props) => {
   const {name, price, image, route, id} =props
-
-  // const products = useSelector(selectProducts);
-  // const dispatch = useDispatch();
-  //
-  // useEffect(() => {
-  //   dispatch(getProducts());
-  // }, [dispatch]);
 
   const [inFavorite, toggleInFavorite] = useToggle();
 
@@ -28,21 +19,12 @@ const ProductItem = (props) => {
 
   return (
     <>
-<<<<<<< HEAD
-      {
-        products.map((item, index) =>
-          <ConteinerItem key={index}>
-            <PhotoBox>
-              <StyledLink to={`/products/${item.name}`}>
-                <Photo alt={item.name} src={item.imageUrl[0]} />
-=======
     {/* <ProductSlider/> */}
 
           <ConteinerItem key={id}>
             <PhotoBox>
               <StyledLink to={`/products/${route}`}>
                 <Photo alt={name} src={image}/>
->>>>>>> Vika
               </StyledLink>
               <ProductActivityContainer>
                 <IconSale />
@@ -53,11 +35,7 @@ const ProductItem = (props) => {
             <TitleBox>
               <StyledLink to={`/products/${route}`}>
                 <NameContainer>
-<<<<<<< HEAD
-                  <Name>{item.name}</Name>
-=======
                   <Name>{name.toUpperCase()}</Name>
->>>>>>> Vika
                 </NameContainer>
               </StyledLink>
               {!inFavorite && <RegularIconFavorite onClick={() => toggleInFavorite(id)} />}
