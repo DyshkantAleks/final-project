e180ac0188c93b50e3bd8f1bceb7613a76b604e2
andrii-myronets <amironets87@gmail.com>
2020-10-27 15:11:39 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import './App.css';
 
-import { ProductCarousel } from './components/ProductCarousel/ProductCarousel';
-import { ProductItem } from './components/ProductItem/ProductItem';
+// import { ProductCarousel } from './components/ProductCarousel/ProductCarousel';
+// import { ProductItem } from './components/ProductItem/ProductItem';
 
 import { Navigation } from './pages/navigation';
 import { getProducts } from './store/products_draft/middlware';
 
 import './styles/style.scss';
 
-function App() {
+function App () {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getProducts());
     return () => {};
-  }, []);
+  }, [dispatch]);
   // const modalIsOpen = useSelector(selectModalIsOpen);
   // const modalContent = useSelector(selectModalContent);
 
