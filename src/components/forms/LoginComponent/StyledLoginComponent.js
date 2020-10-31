3e6413138c0fd6a57e2092963cb68484e1be4a77
DyshkantAleks <o.dushkant@gmail.com>
2020-10-31 16:06:10 +0200

    change form

--- src/components/forms/LoginComponent/StyledLoginComponent.js
+++ src/components/forms/LoginComponent/StyledLoginComponent.js
@@ -1,19 +1,44 @@
 import React from 'react'
 import styled from 'styled-components'
 import {Form} from 'formik'
+import { Title } from '../../Title/Title'
 
+export const CenteredTitle = styled(Title)`
+  text-align: center;
+`
 export const StyledForm = styled(Form)`
 display: flex;
 flex-direction: column;
-height: 130px;
+height: 180px;
 align-items: center;
-justify-content: space-between;
+justify-content: space-around;
 margin: 0 auto;
+font-family: inherit;
+
  div{
    color: red;
  }
 
 input {
-   font-size: 18px;
+   font-size: 14px;
+   padding:10px;
+    :focus{
+     border-color: #afada7;
+     outline: none;
+     ::-webkit-input-placeholder{
+      color: transparent
+     }
+     ::-moz-placeholder{
+      color: transparent
+     }
+  }
+  ::-webkit-input-placeholder, ::-moz-placeholder {
+    font-size: 12px;
+    padding: 1rem;
+    color: #afada7; 
+    font-family: inherit;
+  }
+  
+
 }
 `;
--- src/pages/Account/index.jsx
+++ src/pages/Account/index.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import {Header} from '../../commons/Header/Header';
+import { ContentContairer } from '../../components/Content/Content';
+import { LoginComponent } from '../../components/forms/LoginComponent/LoginComponent';
+import { Title } from '../../components/Title/Title';
+import {GoogleButton} from '../../components/Button/SocialButtons/gogleButton/GoogleButton'
+import styled from 'styled-components';
+import Switch from "react-switch";
 
-export const AccountPage = () => {
+
+export const AccountPage = props => {
+const { handleChange, checked } = props
   return (
-    <>
+    <CenteredDiv>
       <Header/>
-    
-    </>
+      <ContentContairer>
+        <Title/>
+        <LoginComponent/>
+        <label>
+          <p>Я новый пользователь</p>
+          <Switch onChange={handleChange} checked={checked} />
+        </label>
+        <p>или</p>
+        <GoogleButton buttonText='Войти с Google'/>
+      </ContentContairer>
+      
+    </CenteredDiv>
   )
 }
+const CenteredDiv = styled.div`
+  text-align: center;
+`
