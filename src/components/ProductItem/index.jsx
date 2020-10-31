44e61962e93eccc1f00da094e3fd10d22d708b97
miroslava1987 <62344060+miroslava1987@users.noreply.github.com>
2020-10-31 16:08:39 +0200

    Mira. add btnInCart in ProductItem && ProductPage (#31)
    * creating a structure in a database
    
    * creating a structure of img in a database
    
    * add new category in DB
    
    * merge dev
    
    * add all category of img in DB
    
    * Created:
    1. Popular categories list (with items)
    2. Popular products list
    3. Title for all pages
    
    * Add files via upload
    
    * Update .travis.yml (#8)
    
    * Update .travis.yml (#9)
    
    * Popular category Item was deleted from Popular Category List
    
    * add product cart
    
    * fixed app
    
    * install node-sass
    
    * install styled-components
    
    * Current changes
    
    * add swiper in ProductCarousel
    
    * Current changes
    
    * fixed package.json
    
    * change direction in swiper
    
    * Logo added
    Header(mobile menu, static links, users dashboard) added
    Styles fixed
    
    * Category - work in progress
    
    * Catalog in mobile menu - in progress
    
    * Category in progress
    
    * modal & button
    
    * Updated scss for request
    
    * resolve conflicts
    
    * fixed ProductItemDetails
    
    * add useToggle for FontAwesomeIcon
    
    * add PopularProductList in product page
    
    * install react-redux
    
    * merge dev
    
    * fixed merge conflicts
    
    * add button in component
    
    * add button in component
    
    * add button in component
    
    * Current changes
    
    * Current changes
    
    * initial commit
    
    * Styles fixed, favorite categories style fixed
    
    * merge branch dev into Mira
    
    * add change in swiper styles
    
    * clear App.js
    
    * cleare space
    
    * Slider (Homepage) added/ Style updated. Work in progress
    
    * Scss fixed
    
    * fixed package.json
    
    * add ProductPage in router, updated styles, add favorite item. Work in progress
    
    * Travis CI deleted
    
    * delete travis & fixed styles in swiper
    
    * In progress
    Images for slider added
    
    * package.json updated
    
    * Product items fixed
    
    * add media in ProductItem
    
    * commit
    
    * Icons changed, Category in progress, styles in Popular Category (and hover) added
    
    * Current changes
    
    * fixed thumbs in swiper
    
    * fixed swiper-slider
    
    * add media in swiper
    
    * add Navigation in App
    
    * fixed ProductItem after merge dev
    
    * change colorTheme
    
    * merge dev
    
    * add showMore btn and media in product page
    
    * add discountIcon && topRatedIcon
    
    * myronets
    
    * add Counter & fix spaces
    
    * add Counter & fix spaces
    
    * Promotions page added, links in Slider in progress
    
    * clonning
    
    * add screenWidth in product
    
    * add counter in ProductItemDetails
    
    * add brand & availability
    
    * add style in counter & btn
    
    * Content from store added in promotion
    
    * Change UseSelector and useDispatch in Slider. Connect added in Slider
    
    * Slick slider added
    
    * add AllProduct in ProductPage
    
    * myronets
    
    * Clean slider added
    Content fo categories added
    
    * add route in DB and create routing between ProductItem && ProductItemDetails
    
    * add middleware in ProductItem
    
    * add svg in ProductItem
    
    * promo middleware added
    
    * add middlware in ProductItemDetails && components of IconSvg
    
    * add slick-slider
    
    * Items in Categories added from server
    
    * Fixed links in Catalog and Product pages
    
    * delete ProductItemList from ProductPage
    
    * fixed ProductItem
    
    * connection route in ProductPage && ProductSlider
    
    * add change in ProductSlider
    
    * add footer
    
    * myronets
    
    * add footer
    
    * fixed package-lock
    
    * add sale in ProductItem
    
    * add previousPrice in ProductPage
    
    * fix cart
    
    * add Button in ProductItem
    
    * connect Cart with Product
    
    * clear space
    
    * fixed connection with cart
    
    * fixed ProductSlider and styled navigation-button
    
    * clear space and styled ProductPage
    
    * clear non-used components
    
    * fixed thumbs in ProductSlider && add btnInCart in ProductItem
    
    * add btnInCart in Product page
    
    Co-authored-by: Victoria Medvedeva <medvedeva.ufa@gmail.com>
    Co-authored-by: Dyshkant Oleksandr <48221137+DyshkantAleks@users.noreply.github.com>
    Co-authored-by: andrii-myronets <amironets87@gmail.com>

--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { RegularIconFavorite } from './IconsSvg/RegularIconFavorite';
 import { SolidIconFavorite } from './IconsSvg/SolidIconFavorite';
@@ -6,43 +7,62 @@ import { IconSale } from './IconsSvg/IconSale';
 import { IconNew } from './IconsSvg/IconNew';
 import { IconTopRated } from './IconsSvg/IconTopRated';
 import { useToggle } from '../../utils/useToggle';
-import { capitalizeFirstLetter } from '../../utils/capitalizeFirstLetter';
-import { ConteinerItem, PhotoBox, Photo, TitleBox, NameContainer, Name, Price, StyledLink, ProductActivityContainer } from './StyledProductItem';
+import { ConteinerItem, PhotoBox, Photo, TitleBox, NameContainer, Name, Price, StyledLink, ProductActivityContainer, PreviousPrice, PriceContainer, CurrentPrice, ButtonContainer } from './StyledProductItem';
+import { Button } from '../Button';
+import { selectCart } from '../../store/cart/selectors';
+
 
 export const ProductItem = (props) => {
-  const { name, price, image, route, id, isNewProduct, isTopRated } = props
+  const { name, price, image, route, id, isNewProduct, isTopRated, isSale, previousPrice } = props
 
   const [inFavorite, toggleInFavorite] = useToggle();
 
+  const productInCart = useSelector(selectCart);
+
+  const btnInCart = productInCart.map(itemCart => itemCart.product._id).some(itemId => itemId === id);
+
   return (
-    <div>
+    <>
       <ConteinerItem key={id}>
         <PhotoBox>
           <StyledLink to={`/products/${route}`}>
             <Photo alt={name} src={image} />
           </StyledLink>
+          {isSale && (
+            <ProductActivityContainer>
+              <IconSale />
+            </ProductActivityContainer>)}
           {isNewProduct && (
             <ProductActivityContainer>
               <IconNew />
-            </ProductActivityContainer>
-          )}
+            </ProductActivityContainer>)}
           {isTopRated && (
             <ProductActivityContainer>
               <IconTopRated />
-            </ProductActivityContainer>
-          )}
+            </ProductActivityContainer>)}
         </PhotoBox>
         <TitleBox>
           <StyledLink to={`/products/${route}`}>
             <NameContainer>
-              <Name>{capitalizeFirstLetter(`${name}`)}</Name>
+              <Name>{name}</Name>
             </NameContainer>
           </StyledLink>
           {!inFavorite && <RegularIconFavorite onClick={() => toggleInFavorite(id)} />}
           {inFavorite && <SolidIconFavorite onClick={() => toggleInFavorite(id)} />}
-          <Price>{price.toLocaleString()}</Price>
+          {isSale &&
+            <PriceContainer>
+              <CurrentPrice>{price.toLocaleString()}</CurrentPrice>
+              <PreviousPrice>{previousPrice.toLocaleString()}</PreviousPrice>
+            </PriceContainer>}
+          {!isSale &&
+            <PriceContainer>
+              <Price>{price.toLocaleString()}</Price>
+            </PriceContainer>}
+          <ButtonContainer>
+            {btnInCart ? <Button text={'В корзине'} /> : <Button text={'Купить'} />}
+          </ButtonContainer>
         </TitleBox>
       </ConteinerItem>
-    </div>
+    </>
   )
 };
--- src/components/TopRatedProducts/TopRatedProductsList.jsx
+++ src/components/TopRatedProducts/TopRatedProductsList.jsx
@@ -1,16 +1,11 @@
-import React, {useEffect} from 'react';
-import styled from 'styled-components';
-import {Title} from '../Title/Title';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import {device} from '../../styles/breakpoints/breakpoints';
-//import {ProductItemList} from "../ProductItemDetails/StyledProductItemDetails";
-import {useDispatch, useSelector} from "react-redux";
-import {selectProductsForFilter} from "../../store/products_draft/selectors";
-
-import {getProductsByIsPopular} from "../../store/products_draft/middlware";
-
-import {ProductItem} from "../ProductItem";
-import {ProductItemList} from "../../pages/Product/StyledProductPage";
+import { Title } from '../Title/Title';
+import { selectProductsForFilter } from '../../store/products_draft/selectors';
+import { getProductsByIsPopular } from '../../store/products_draft/middlware';
+import { ProductItem } from '../ProductItem';
+import { ProductItemList } from '../../pages/Product/StyledProductPage';
 
 
 export const TopRatedProductsList = () => {
@@ -19,17 +14,17 @@ export const TopRatedProductsList = () => {
 
   useEffect(() => {
     dispatch(getProductsByIsPopular());
-  }, []);
+  }, [dispatch]);
 
   console.log(selectByIsPopular.products);
 
   return (
     <>
-      <Title text={'Популярные товары'}/>
+      <Title text={'Популярные товары'} />
       <ProductItemList>
         {
-           selectByIsPopular.products.map((e) => (
-             <ProductItem name={e.name} price={e.currentPrice} image={e.imageUrl[0]} key={e.id} route={e.route} id={e._id}/>
+          selectByIsPopular.products.map((e) => (
+            <ProductItem name={e.name} price={e.currentPrice} image={e.imageUrl[0]} key={e.id} route={e.route} id={e._id} />
           ))
         }
       </ProductItemList>
