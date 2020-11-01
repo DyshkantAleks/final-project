0198583d7e940f43ae811ae5a1f5512e9878be47
Kateryna Sosnikova <sosnikova@gmail.com>
2020-11-01 18:05:15 +0200

    one more commit

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import {Header} from '../../commons/Header/Header';
 import {AboutText} from "./AboutPageComponents/AboutText";
-<<<<<<< HEAD
-import { ContentContairer } from '../../components/Content/Content';
-=======
 import {Footer} from "../../commons/Footer";
+import { ContentContairer } from '../../components/Content/Content';
 
->>>>>>> dev
 
 export const AboutPage = () => {
   return (
     <ContentContairer>
       <Header/>
       <AboutText/>
-<<<<<<< HEAD
-    </ContentContairer>
-=======
       <Footer/>
-    </>
->>>>>>> dev
+      </ContentContairer>
   )
 }
--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,26 +1,18 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 //import styled from 'styled-components';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectFavorites } from '../../store/favorites/selectors';
 import { Header } from '../../commons/Header/Header';
-//import { Button } from '../../components/Button';
 import { Title } from '../../components/Title/Title';
 import { CartContainer } from '../Cart/'
 import { CartItem } from '../Cart/CartItem';
-import { selectProducts } from '../../store/products_draft/selectors';
-import { getProductList } from '../../store/products_draft/actions';
 import { ContentContairer } from '../../components/Content/Content';
 
 export const FavoritesPage = () => {
-  const dispatch = useDispatch()
-  // temporary
-  useEffect(() => {
-    dispatch(getProductList())
-  }, [dispatch]);
 
   const favorites = useSelector(selectFavorites);
-  const products = useSelector(selectProducts);
+ 
 
   return (
     <ContentContairer>
