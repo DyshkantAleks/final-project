8f36d480a337a23fa31193e20ff473b628c2210f
miroslava1987 <miroslava1987@gmail.com>
2020-11-02 21:28:17 +0200

    fixed Button

--- src/components/Button/StyledButton.jsx
+++ src/components/Button/StyledButton.jsx
@@ -1,19 +1,19 @@
 import styled from 'styled-components';
 
 export const StyledButton = styled.button`
-border: .1rem solid #7191A6;
+width: ${props => props.width ? '12rem' : '20rem'};
+font-weight: bold;
 color: ${props => props.color ? '#fff' : '#7191A6'};
 background-color: ${props => props.color ? '#7191A6' : '#fff'};
-width: ${props => props.width ? '12rem' : '20rem'};
-padding: 10px 20px;
-border-radius: 5px;
-font-weight: bold;
+border: .1rem solid #7191A6;
+padding: 1rem 2rem;
+border-radius: .5rem;
 transition: all .3s linear;
   &:hover{
     cursor: pointer;
     color: ${props => props.color ? '#7191A6' : '#fff'};
     background-color: ${props => props.color ? '#fff' : '#7191A6'};
-    border: 1px solid #7191A6;
+    border: .1rem solid #7191A6;
 };
   &:focus{
     outline: none;
@@ -21,10 +21,10 @@ transition: all .3s linear;
   &:disabled{
     background-color:#e6e9ef;
     color: #6F7281;
-    border: 1px solid #e6e9ef;
+    border: .1rem solid #e6e9ef;
     cursor: default;
         &:hover{
             color: #6F7281;
-        }
-  }
+        };
+  };
 `;
