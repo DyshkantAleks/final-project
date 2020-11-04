84807ed4efe6436d372d990d733b4e4177bc5410
ivimedv <48404885+ivimedv@users.noreply.github.com>
2020-11-04 08:20:48 +0200

    Vika (#35)
    * creating a structure in a database
    
    * creating a structure of img in a database
    
    * add new category in DB
    
    * merge dev
    
    * add all category of img in DB
    
    * Created:
    1. Popular categories list (with items)
    2. Popular products list
    3. Title for all pages
    
    * Add files via upload
    
    * Update .travis.yml (#8)
    
    * Update .travis.yml (#9)
    
    * Popular category Item was deleted from Popular Category List
    
    * add product cart
    
    * fixed app
    
    * install node-sass
    
    * install styled-components
    
    * Current changes
    
    * add swiper in ProductCarousel
    
    * Current changes
    
    * fixed package.json
    
    * change direction in swiper
    
    * Logo added
    Header(mobile menu, static links, users dashboard) added
    Styles fixed
    
    * Category - work in progress
    
    * Catalog in mobile menu - in progress
    
    * Category in progress
    
    * modal & button
    
    * Updated scss for request
    
    * resolve conflicts
    
    * fixed ProductItemDetails
    
    * add useToggle for FontAwesomeIcon
    
    * add PopularProductList in product page
    
    * install react-redux
    
    * merge dev
    
    * fixed merge conflicts
    
    * add button in component
    
    * add button in component
    
    * add button in component
    
    * Current changes
    
    * Current changes
    
    * initial commit
    
    * Styles fixed, favorite categories style fixed
    
    * merge branch dev into Mira
    
    * add change in swiper styles
    
    * clear App.js
    
    * cleare space
    
    * Slider (Homepage) added/ Style updated. Work in progress
    
    * Scss fixed
    
    * fixed package.json
    
    * add ProductPage in router, updated styles, add favorite item. Work in progress
    
    * Travis CI deleted
    
    * delete travis & fixed styles in swiper
    
    * In progress
    Images for slider added
    
    * package.json updated
    
    * Product items fixed
    
    * add media in ProductItem
    
    * commit
    
    * Icons changed, Category in progress, styles in Popular Category (and hover) added
    
    * Current changes
    
    * fixed thumbs in swiper
    
    * fixed swiper-slider
    
    * add media in swiper
    
    * add Navigation in App
    
    * fixed ProductItem after merge dev
    
    * change colorTheme
    
    * merge dev
    
    * add showMore btn and media in product page
    
    * add discountIcon && topRatedIcon
    
    * myronets
    
    * add Counter & fix spaces
    
    * add Counter & fix spaces
    
    * Promotions page added, links in Slider in progress
    
    * clonning
    
    * add screenWidth in product
    
    * add counter in ProductItemDetails
    
    * add brand & availability
    
    * add style in counter & btn
    
    * Content from store added in promotion
    
    * Change UseSelector and useDispatch in Slider. Connect added in Slider
    
    * Slick slider added
    
    * add AllProduct in ProductPage
    
    * Clean slider added
    Content fo categories added
    
    * add route in DB and create routing between ProductItem && ProductItemDetails
    
    * add middleware in ProductItem
    
    * add svg in ProductItem
    
    * promo middleware added
    
    * add middlware in ProductItemDetails && components of IconSvg
    
    * add slick-slider
    
    * Items in Categories added from server
    
    * Fixed links in Catalog and Product pages
    
    * Popular categories fixed, images in DG fixed, name of product changed in DB
    
    * changes in Catalog added
    
    * onsole.log deleted
    Keys in components added
    
    * Content for static pages added
    
    * Dots and arrows style in Slider changed
    
    * com
    
    * JS style code
    
    * search input added
    
    * title in Search page added
    
    Co-authored-by: miroslava1987 <miroslava1987@gmail.com>
    Co-authored-by: Dyshkant Oleksandr <48221137+DyshkantAleks@users.noreply.github.com>
    Co-authored-by: andrii-myronets <amironets87@gmail.com>

--- src/pages/ProductList/ProductListPage.jsx
+++ src/pages/ProductList/ProductListPage.jsx
@@ -4,24 +4,25 @@ import { Header } from '../../commons/Header/Header'
 import { selectProducts } from '../../store/products_draft/selectors'
 import { selectCategoryFromRoute } from '../../store/categories/selectors'
 import { ProductItem } from '../../components/ProductItem'
-import { ContentContairer } from '../../components/Content/Content';
-import { ProductItemList } from '../Product/StyledProductPage';
-import { Footer } from '../../commons/Footer';
+import { ContentContairer } from '../../components/Content/Content'
+import { ProductItemList } from '../Product/StyledProductPage'
+import { Footer } from '../../commons/Footer'
 
 export const ProductListPage = ({ match, location }) => {
   const { params: { route } } = match
 
   const currentItemByRoute = useSelector(selectCategoryFromRoute(route))
 
-  const allProducts = useSelector(selectProducts);
-  let array = [];
+  const allProducts = useSelector(selectProducts)
+
+  let array = []
   if (currentItemByRoute) {
-    const isRootCategory = currentItemByRoute.parentId === 'null';
+    const isRootCategory = currentItemByRoute.parentId === 'null'
     array = allProducts.filter(e => isRootCategory
       ? e.category === currentItemByRoute.category
       : e.subCategory === currentItemByRoute.category
-    );
-  };
+    )
+  }
 
   return (
 
