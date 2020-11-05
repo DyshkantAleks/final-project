fb1c44cc14d281a8ea3cdf39deba72bd2b7d6d05
Victoria Medvedeva <medvedeva.ufa@gmail.com>
2020-11-05 12:26:59 +0200

    merge conflict resolved

--- src/components/ScrollToTop/index.jsx
+++ src/components/ScrollToTop/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useWindowScroll } from 'react-use';
+import { useWindowScroll } from 'react-use'
 
 import { ScrollIcon } from './ScrollIcon';
 import { ScrollToTopContainer } from './StyledScrollToTop';
