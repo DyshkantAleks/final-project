3a32603a58b23d5add0b02a4bbd7fcb6685d3f28
Roman_Semywolos <Roman.Semywolos@gmail.com>
2020-11-13 12:05:40 +0200

    trying to get a response from the server

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -6,11 +6,40 @@ import {ContactForm} from '../../components/forms/ContactForm/ContactForm';
 import {OrderCart} from './OrderCart';
 import { Title } from '../../components/Title/Title'
 import { ContentContairer } from '../../components/Content/Content'
+import { server } from '../../API'
 
 export const OrderPage = () => {
   const handleSubmit = (values) => {
-    console.log(values)
+
+    const myOrder = {
+      deliveryAddress: {
+        city: values.city,
+        address: values.street + " " + values.house,
+      },
+      email: values.email,
+      mobile: values.phone,
+      letterSubject: "Thank you for order! You are welcome!",
+      letterHtml:
+          "<h1>Your order is placed. OrderNo is ...</h1><p>{Other details about order in your HTML}</p>"
+    };
+    console.log(myOrder)
+
+    const createNewOrder = (newOrder) => async (dispatch) => {
+      try {
+        console.log(newOrder)
+        const {status, data} = await server.post('/orders', JSON.stringify(newOrder))
+        if (status === 200) {
+        console.log("status === 200")
+        console.log(status, data)
+        }
+          } 
+      catch (error) {
+        }
+    }
+    createNewOrder(myOrder)();
   }
+
+  
   return (
     <ContentContairer>
       <Header/>
