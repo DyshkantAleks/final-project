9df50a711770024d77c80d6c572bc09e0d46b847
andrii-myronets <amironets87@gmail.com>
2020-11-13 22:28:08 +0200

    fixed filters

--- src/pages/ProductList/ProductListPage.jsx
+++ src/pages/ProductList/ProductListPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import {Content, Wrapper, Title, FilterName, StyledCheckboxGroupe, ProductList} from './StyledProductListPage';
@@ -19,34 +19,41 @@ import { ScrollToTop } from '../../components/ScrollToTop';
 export const ProductListPage = ({ match }) => {
   const [checkedColors, setCheckedColors] = useState([]);
   const [checkedBrands, setCheckedBrands] = useState([]);
+  const [priceValues, setPriceValues] = useState([]);
 
   const { params: { route } } = match;
-
+  
   const currentItemByRoute = useSelector(selectCategoryFromRoute(route));
   const allProducts = useSelector(selectProducts);
   const array = categoriesFilter(allProducts, currentItemByRoute);
-
+   
   const arrayOfColors = new Set(array.map(item => item.color));
   const arrayOfBrands = new Set(array.map(item => item.brand));
 
-  const onCheckHandlerColor = (checkedValues) => {
-    setCheckedColors(checkedValues)
-    console.log(checkedValues)
-  }
-  const onCheckHandlerBrand = (checkedValues) => {
-    setCheckedBrands(checkedValues)
-  console.log(checkedColors.lenght)
-  console.log(checkedValues)
+  useEffect(() => {
+    setCheckedColors([]);
+    setCheckedBrands([]);
+  }, [route]);
+  console.log(priceValues);
+  const newArray = array
+    .filter(productItem => (checkedColors.length === 0) ? productItem : checkedColors.some(chackedItem => chackedItem === productItem.color))
+    .filter(productItem => (checkedBrands.length === 0) ? productItem : checkedBrands.some(chackedItem => chackedItem === productItem.brand))
+    .filter(productItem => (priceValues.length === 0) ? productItem : (priceValues[0] < productItem.currentPrice && productItem.currentPrice < priceValues[1]))
 
+  const onChackedColorHandler = (checkedValues) => {
+    setCheckedColors(checkedValues)
+  }
+  const onCheckedBrandHandler = (checkedValues) => {
+    setCheckedBrands(checkedValues)
+  }
+  const onAfterChangeHandler = (disabled) => {
+    setPriceValues(disabled)
   }
   
-  console.log(checkedColors.lenght)
-  console.log(checkedBrands.lenght)
-
   return (
     <>
-      <Header />
-      <ScrollToTop />
+      <Header/>
+      <ScrollToTop/>
       <ContentContairer>
         <Content>
           <Wrapper>
@@ -56,37 +63,28 @@ export const ProductListPage = ({ match }) => {
               currency="грн"
               min={0}
               max={200000}
-              step={1}
-              defaultValue={50000}
-            // onAfterChangeHandler={onAfterChangeHandler}
+              step={100}
+              defaultValue={[1000, 100000]}
+              onAfterChangeHandler={onAfterChangeHandler}
             />
 
-            <StyledCheckboxGroupe onChange={onCheckHandlerColor} >
+            <StyledCheckboxGroupe onChange={onChackedColorHandler}>
               <FilterName>Цвет</FilterName>
-              {[...arrayOfColors].map((item, index) => (
-                <StyledCheckbox
-                  key={index}
-                  // onClickHandler={onClickHandlerColor}
-                  // filterOption={item}
-                  value={item}
-                >{item}</StyledCheckbox>)
+              {[...arrayOfColors].map((item, index) =>
+                <StyledCheckbox key={index} value={item}>{ item }</StyledCheckbox>
               )}
             </StyledCheckboxGroupe>
-            <StyledCheckboxGroupe onChange={onCheckHandlerBrand} >
+            <StyledCheckboxGroupe onChange={onCheckedBrandHandler}>
               <FilterName>Бренд</FilterName>
-              {[...arrayOfBrands].map((item, index) => (
-                <StyledCheckbox
-                  key={index}
-                  // onClickHandler={onClickHandlerColor}
-                  value={item}
-                >{item}</StyledCheckbox>)
+              {[...arrayOfBrands].map((item, index) =>
+                <StyledCheckbox key={index} value={item}>{ item }</StyledCheckbox>
               )}
             </StyledCheckboxGroupe>
 
           </Wrapper>
           {/* <ProductSorting onChangeHandler={onChangeHandler} /> */}
           <ProductList>
-            {array.map((e, index) => (
+            {newArray.map((e, index) => (
               <ProductItem
                 key={index}
                 name={e.name}
