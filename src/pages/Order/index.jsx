58214df3105741473f065a3b5ff5000ef6546ed4
miroslava1987 <miroslava1987@gmail.com>
2020-11-16 04:15:28 +0200

    merge dev into Mira

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import styled from 'styled-components'
-import { device } from '../../styles/breakpoints/breakpoints'
+import styled from 'styled-components';
+
+import { device } from '../../styles/breakpoints/breakpoints';
 import { Header } from '../../commons/Header/Header';
 import { ContactForm } from '../../components/forms/ContactForm/ContactForm';
 import { OrderCart } from './OrderCart';
-import { Title } from '../../components/Title/Title'
-import { ContentContairer } from '../../styles/GeneralStyledComponents'
+import { Title } from '../../components/Title/Title';
 import { handleSubmit } from './SendNewOrder';
+import { ContentContainer } from '../../styles/GeneralStyledComponents';
 
 export const OrderPage = () => {
 
   return (
     <ContentContainer>
-      <Header/>
+      <Header />
       <Title text='Оформить заказ' />
       <ContainerPage>
-        <ContactForm handleSubmit={handleSubmit}/>
-        <OrderCart/>
+        <ContactForm handleSubmit={handleSubmit} />
+        <OrderCart />
       </ContainerPage>
     </ContentContainer>
   )
@@ -25,8 +26,8 @@ export const OrderPage = () => {
 const ContainerPage = styled.div`
 display: flex;
 flex-direction: column;
-@media${device.tabletM}{
-justify-content: space-between;
-flex-direction: row;
-}
-`
+  @media ${device.tabletM}{
+    justify-content: space-between;
+    flex-direction: row;
+  };
+`;
