2b837398eddf3fa64dc688ef440304c30d897d69
Dyshkant Oleksandr <48221137+DyshkantAleks@users.noreply.github.com>
2020-11-17 16:12:30 +0200

    Alex (#67)
    * update order
    
    * udate
    
    * add checkout order quantity
    
    * update
    
    * update order
    
    * delete console.log

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -1,23 +1,24 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
 
-import { device } from '../../styles/breakpoints/breakpoints';
-import { Header } from '../../commons/Header/Header';
-import { Footer } from '../../commons/Footer';
-import { ContactForm } from '../../components/forms/ContactForm/ContactForm';
-import { OrderCart } from './OrderCart';
+import { device } from "../../styles/breakpoints/breakpoints";
+import { Header } from "../../commons/Header/Header";
+import { Footer } from "../../commons/Footer";
+import { ContactForm } from "../../components/forms/ContactForm/ContactForm";
+import { OrderCart } from "./OrderCart";
 
-import { Title } from '../../components/Title/Title';
-import { ContentContainer } from '../../styles/GeneralStyledComponents';
+import { Title } from "../../components/Title/Title";
+import { ContentContainer } from "../../styles/GeneralStyledComponents";
 
-import { confirmOrder } from '../../store/order/middlware';
-import { getProducts } from '../../store/products/middlware';
-import { checkQuantity } from '../../store/cart/middlware';
-import { selectProducts } from '../../store/products/selectors';
-import { selectCart } from '../../store/cart/selectors';
-import { openModal } from '../../store/modal/actions-creators';
-import { ScrollToTop }from '../../components/ScrollToTop';
+import { confirmOrder } from "../../store/order/middlware";
+import { getProducts } from "../../store/products/middlware";
+import { checkQuantity } from "../../store/cart/middlware";
+import { selectProducts } from "../../store/products/selectors";
+import { selectCart } from "../../store/cart/selectors";
+import { openModal } from "../../store/modal/actions-creators";
+import { selectOrder } from "../../store/order/selectors";
+import { ScrollToTop } from "../../components/ScrollToTop";
 
 export const OrderPage = (props) => {
   const dispatch = useDispatch();
@@ -27,20 +28,23 @@ export const OrderPage = (props) => {
   }, []);
   const products = useSelector(selectProducts);
   const cart = useSelector(selectCart);
+  const order = useSelector(selectOrder);
   const shortageProducts = checkQuantity(products, cart);
 
-  if (shortageProducts.length) {    
-
+  if (shortageProducts.length) {
     dispatch(
       openModal({
         content: shortageProducts.map((item) => (
           <ModalContentWrapper>
-            <ModalTitle>Товара 
-              <ItemInnerWrapper> {item.product.name.toLowerCase()} </ItemInnerWrapper>
-                не хватает на складе. <br/>
-                </ModalTitle>
-                <ModalTitle>Доступно в количестве {item.quantity}</ModalTitle>
-           
+            <ModalTitle>
+              Товара
+              <ItemInnerWrapper>
+                {" "}
+                {item.product.name.toLowerCase()}{" "}
+              </ItemInnerWrapper>
+              не хватает на складе. <br />
+            </ModalTitle>
+            <ModalTitle>Доступно в количестве {item.quantity}</ModalTitle>
           </ModalContentWrapper>
         )),
       })
@@ -48,38 +52,43 @@ export const OrderPage = (props) => {
   }
   return (
     <>
-    <Header />
-    <ScrollToTop/>
-    <ContentContainer>
+      <Header />
+      <ScrollToTop />
+      <ContentContainer>
+        <Title text="Оформить заказ" />
 
+        <ContainerPage>
+          <ComponentContainer>
+            <ContactForm
+              handleSubmit={(val) => {
+                dispatch(confirmOrder(val));
+                setTimeout(
+                  () =>
+                    dispatch(
+                      openModal({
+                        content: <h2> Ваш заказ № {order.orderNo} принят</h2>,
+                      })
+                    ),
+                  500
+                );
+              }}
+            />
 
-      <Title text='Оформить заказ' />
-
-      <ContainerPage>
-      <ComponentContainer>
-        <ContactForm
-          handleSubmit={(val) => {
-            dispatch(confirmOrder(val));
-          }}
-        />
-      </ComponentContainer>
-      <ComponentContainer>
-        <OrderCart />
-      </ComponentContainer>
-      </ContainerPage>
-    </ContentContainer>
-    <Footer/>
+            <OrderCart />
+          </ComponentContainer>
+        </ContainerPage>
+      </ContentContainer>
+      <Footer />
     </>
-
   );
 };
 
 const ComponentContainer = styled.div`
-@media ${device.desktop} {
-  margin-left: 1em;
-  margin-right: 1em;
-  margin-bottom: 1em;
-} ;
+  @media ${device.desktop} {
+    margin-left: 1em;
+    margin-right: 1em;
+    margin-bottom: 1em;
+  } ;
 `;
 
 const ContainerPage = styled.div`
@@ -91,18 +100,17 @@ const ContainerPage = styled.div`
   } ;
 `;
 
-
 const ModalContentWrapper = styled.div`
-padding: 3rem 1.5rem;
-text-align: center
-`
+  padding: 3rem 1.5rem;
+  text-align: center;
+`;
 
 const ModalTitle = styled.h2`
-font-size: 1.1em;
-margin-bottom: 2rem;
-`
+  font-size: 1.1em;
+  margin-bottom: 2rem;
+`;
 
 const ItemInnerWrapper = styled.span`
-color: #7191A6;
-font-weight: 700;
-`
+  color: #7191a6;
+  font-weight: 700;
+`;
