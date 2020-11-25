2ff5b66343d52fc173ee6d8d146a02b8268f394e
Tanya Krasnonos <krasnonos.tv@gmail.com>
2020-11-25 16:03:42 +0200

    changed styles

--- src/pages/ProductList/ProductListPage.jsx
+++ src/pages/ProductList/ProductListPage.jsx
@@ -3,15 +3,14 @@ import { useSelector } from 'react-redux';
 import { FilterTwoTone } from '@ant-design/icons';
 import { Collapse } from 'antd';
 import { slide as MobileFilter } from 'react-burger-menu';
-import './style.scss';
 
 import { Content, Wrapper, FilterName, StyledCheckboxGroupe, ProductList, FiltersWrapper, StyledPagination } from './StyledProductListPage';
 import { selectProducts } from '../../store/products/selectors';
 import { selectCategoryFromRoute } from '../../store/categories/selectors';
 import { ProductItem } from '../../components/ProductItem';
 import { ContentContainer } from '../../styles/GeneralStyledComponents';
-import { RangeSlider } from '../../components/rangeSlider/RangeSlider';
-import { ProductSorting } from '../../components/productSorting/ProductSorting'
+import { RangeSlider } from '../../components/RangeSlider/RangeSlider';
+import { ProductSorting } from '../../components/ProductSorting/ProductSorting'
 import { categoriesFilter } from '../../utils/filters';
 import { StyledCheckbox } from '../../components/CheckBox/StyledCheckboxFilter';
 import useWindowDimensions from '../../utils/useWindowDimensions';
@@ -20,13 +19,8 @@ export const ProductListPage = ({ match }) => {
   const { Panel } = Collapse;
   const { screenWidth } = useWindowDimensions();
 
-<<<<<<< HEAD
-  const [minValue, setMinValue] = useState(0);
-  const [maxValue, setMaxValue] = useState(9);
-=======
-  let pageSize = 9;
+  const pageSize = 9;
   const [current, setCurrent] = useState(1);
->>>>>>> dev
 
   const [checkedColors, setCheckedColors] = useState([]);
   const [checkedBrands, setCheckedBrands] = useState([]);
@@ -84,108 +78,6 @@ export const ProductListPage = ({ match }) => {
   const onSelectChangeHandler = (checkedSelectValue) => {
     setSortValue(checkedSelectValue)
   }
-<<<<<<< HEAD
-
-  const onPaginationChange = value => {
-    if (value <= 1) {
-      setMinValue(0);
-      setMaxValue(9);
-    } else {
-      setMinValue(maxValue);
-      setMaxValue(value * 9);
-    }
-  }
-
-  return (
-    <>
-      <Header />
-      <ScrollToTop />
-      <ContentContainer>
-        <Content>
-          {screenWidth >= 1200 && <Wrapper>
-            <Title>Название</Title>
-            <RangeSlider
-              price='Цена'
-              currency='грн'
-              min={0}
-              max={200000}
-              step={100}
-              defaultValue={[1000, 160000]}
-              onAfterChangeHandler={onAfterChangeHandler}
-            />
-
-            <StyledCheckboxGroupe onChange={onChackedColorHandler} value={checkedColors}>
-              <FilterName>Цвет</FilterName>
-              {[...arrayOfColors].map((item, index) =>
-                <StyledCheckbox key={index} value={item}>{item}</StyledCheckbox>
-              )}
-            </StyledCheckboxGroupe>
-
-            <StyledCheckboxGroupe onChange={onCheckedBrandHandler} value={checkedBrands}>
-              <FilterName>Бренд</FilterName>
-              {[...arrayOfBrands].map((item, index) =>
-                <StyledCheckbox key={index} value={item}>{item}</StyledCheckbox>
-              )}
-            </StyledCheckboxGroupe>
-          </Wrapper>}
-
-          <Wrapper>
-            <FiltersWrapper>
-              <ProductSorting onChangeHandler={onSelectChangeHandler} value={sortValue} />
-              {screenWidth <= 1200 && <Wrapper>
-                <MobileFilter disableAutoFocus customBurgerIcon={<FilterTwoTone twoToneColor="#7191a6" />}>
-                  <Collapse ghost>
-                    <Panel header="Цена" key="1">
-                      <RangeSlider
-                        min={0}
-                        max={200000}
-                        step={100}
-                        defaultValue={[1000, 160000]}
-                        onAfterChangeHandler={onAfterChangeHandler}
-                      />
-                    </Panel>
-                    <Panel header="Цвет" key="2">
-                      <StyledCheckboxGroupe onChange={onChackedColorHandler} value={checkedColors}>
-                        {[...arrayOfColors].map((item, index) =>
-                          <StyledCheckbox key={index}>{item}</StyledCheckbox>
-                        )}
-                      </StyledCheckboxGroupe>
-                    </Panel>
-                    <Panel header="Бренд" key="3">
-                      <StyledCheckboxGroupe onChange={onCheckedBrandHandler} value={checkedBrands}>
-                        {[...arrayOfBrands].map((item, index) =>
-                          <StyledCheckbox key={index}>{item}</StyledCheckbox>
-                        )}
-                      </StyledCheckboxGroupe>
-                    </Panel>
-                  </Collapse>
-                </MobileFilter>
-              </Wrapper>}
-            </FiltersWrapper>
-            <ProductList>
-              {result.slice(minValue, maxValue).map((e, index) => (
-                <ProductItem
-                  key={index}
-                  name={e.name}
-                  price={e.currentPrice}
-                  previousPrice={e.previousPrice}
-                  image={e.imageUrl[0]}
-                  route={e.route}
-                  id={e._id}
-                  isNewProduct={e.isNewProduct}
-                  isTopRated={e.isTopRated}
-                  isSale={e.isSale}
-                  product={e}
-                />
-              ))}
-            </ProductList>
-            <StyledPagination defaultCurrent={1} defaultPageSize={9} total={result.length} onChange={onPaginationChange} />
-          </Wrapper>
-        </Content>
-      </ContentContainer>
-      <Footer />
-    </>
-=======
   const prodFilterNotFound = () => {
     return (
       <div>По фильтрам {checkedColors} и {checkedBrands} ничего не найдено.</div>
@@ -254,6 +146,5 @@ export const ProductListPage = ({ match }) => {
         </Wrapper>
       </Content>
     </ContentContainer>
->>>>>>> dev
   );
 }
