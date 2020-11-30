a0261c07a5de5effd1a5074e12994530156e57e9
miroslava1987 <miroslava1987@gmail.com>
2020-11-30 14:47:54 +0200

    clear space in public and in my components

--- src/store/products/operations.jsx
+++ src/store/products/operations.jsx
@@ -22,16 +22,15 @@ export const getProductsByIsPopular = () => async (dispatch) => {
   } catch (error) {
     console.log(error.response.data)
   }
-}
+};
 
 export const getProductsByIsNew = () => async (dispatch) => {
   try {
     const { status, data } = await server('/products/filter?isNew=true')
-    // console.log(data)
     if (status === 200) {
       dispatch(setProducts(data.products))
     }
   } catch (error) {
     console.log(error)
   }
-}
+};
