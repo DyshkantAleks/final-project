69de515ed2762f095195eed88a90be39fb696324
Kateryna Sosnikova <sosnikova@gmail.com>
2020-12-02 18:24:12 +0200

    final commit for kateryna

--- src/store/promotions/actions.jsx
+++ src/store/promotions/actions.jsx
@@ -4,17 +4,3 @@ export const setPromotions = payload => ({
   type: SET_PROMOTIONS,
   payload
 })
-<<<<<<< HEAD
-
-// export const getPromotionsList = () => async dispatch => {
-//     try {
-//             const res = await fetch('/db_promo.json');
-//             const data = await res.json();
-//             dispatch(setPromotions(data.promotions));
-//
-//     } catch (err) {
-//         console.log(err);
-//     }
-// };
-=======
->>>>>>> dev
